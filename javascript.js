2ac5766d08d6b117ce9b517771bd4c1e550cb023
Eric <anderson.ericandrew@gmail.com>
2023-07-11 16:48:54 -0700

    add js logic for coloring in grid as well as clearing the board. also add css styling for grid and knobs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
 const grid = document.querySelector('.grid');
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 3750; i++) {
   const gridCell = document.createElement('div')
+
   gridCell.classList.add('grid-cell')
-  gridCell.textContent = 'test'
+
+  gridCell.addEventListener('mouseover', () => {
+    gridCell.style.background = "black"
+  })
+
   grid.appendChild(gridCell)
 }
+
+const clearBoard = () => {
+  const allItems = document.querySelectorAll('.grid-cell')
+  allItems.forEach((item) => {
+    item.style.background = "rgb(199, 199, 199)"
+  })
+}
+
+const reset = document.querySelector('#left-circle')
+reset.addEventListener("click", () => clearBoard())
